86f4d7d92279dbe65576e2564b1d936e6364aab8
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2013-10-13 12:24:15 +1100

    sync classes

--- angular-bootstrap-select.js
+++ angular-bootstrap-select.js
@@ -5,9 +5,6 @@ angular.module('angular-bootstrap-select', [])
 	return {
 		restrict: 'AE',
 		require: ['?ngModel'],
-		controller: function($scope, $element, $attrs) {
-			$scope.dropdown = {};
-		},
 		link: function(scope, element, attrs, ctrl) {
 
 			if(!ctrl[0]) {
@@ -17,22 +14,27 @@ angular.module('angular-bootstrap-select', [])
 
 			var BOOTSTRAP_SELECT_DATA = '$bootstrapSelect';
 			var ngModelCtrl = ctrl[0];
-			var template = "<div class=\"bootstrap-select\">\n" +
-    "	<div class=\"btn-group\">\n" +
-    "		<button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">\n" +
-    "			<span class=\"selected\">{{ dropdown.selected.label }}</span>\n" +
-    "			<span class=\"caret\"></span>\n" +
-    "		</button>\n" +
-    "		<ul class=\"dropdown-menu\" role=\"menu\">\n" +
-    "			<li role=\"presentation\" ng-repeat=\"option in $options\" ng-click=\"dropdown.selected = option; updateModel()\">\n" +
-    "				<a role=\"menuitem\" tab-index=\"-1\">{{ option.label }}</a>\n" +
-    "			</li>\n" +
-    "		</ul>\n" +
-    "	</div>\n" +
+			var template =  "<div class=\"btn-group bootstrap-select\" ng-init=\"current = {}\">\n" +
+    "	<button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "		<span class=\"selected\">{{ current.selection.label }}</span>\n" +
+    "		<span class=\"caret\"></span>\n" +
+    "	</button>\n" +
+    "	<ul class=\"dropdown-menu\" role=\"menu\">\n" +
+    "		<li role=\"presentation\" ng-repeat=\"option in $options\" ng-click=\"current.selection = option\">\n" +
+    "			<a role=\"menuitem\" tab-index=\"-1\">{{ option.label }}</a>\n" +
+    "		</li>\n" +
+    "	</ul>\n" +
     "</div>";
 
 			var dropdown = angular.element(template);
-			$compile(dropdown)(scope);
+			var dropdownScope = scope.$new(true);
+			$compile(dropdown)(dropdownScope);
+			// update the classes on the dropdown container whenever they change on the select
+			var initialClasses = dropdown[0].className;
+			attrs.$observe('class', function(classes) {
+				dropdown[0].className = initialClasses + " " + classes;
+			});
+
 			element.after(dropdown);
 			element.css('display', 'none');
 
@@ -46,25 +48,24 @@ angular.module('angular-bootstrap-select', [])
 
 			var buildObjFromOptionElement = function( optionEl ) {
 				return {
-					value: optionEl.val(),
-					label: optionEl.text(),
-					selected: optionEl.prop('selected')
+					index: optionEl.val(),
+					label: optionEl.text()
 				};
 			};
 
 			var updateOptions = function( items ) {
 				var options = element.find('option');
-				scope.$options = [];
+				dropdownScope.$options = [];
 				angular.forEach(options, function(option) {
-					o = angular.element(option);
+					var o = angular.element(option);
 					var data = buildObjFromOptionElement(o);
-					if( items && items[data.value] ) {
-						data.model = items[data.value];
+					if( items && items[data.index] ) {
+						data.value = items[data.index];
 					} else {
-						data.model = null;
+						data.value = null;
 					}
 					o.data(BOOTSTRAP_SELECT_DATA, data);
-					scope.$options.push(data);
+					dropdownScope.$options.push(data);
 				});
 			};
 
@@ -73,7 +74,7 @@ angular.module('angular-bootstrap-select', [])
 				angular.forEach(options, function(option) {
 					var o = angular.element(option);
 					if( element.val() === o.val() ) {
-						scope.dropdown.selected = o.data(BOOTSTRAP_SELECT_DATA);
+						dropdownScope.current.selection = o.data(BOOTSTRAP_SELECT_DATA);
 						return;
 					}
 				});
@@ -86,12 +87,12 @@ angular.module('angular-bootstrap-select', [])
 			});
 
 			// update the select and the model when our selection changes
-			scope.updateModel = function() {
-				var locals = {};
-				locals[valueName] = scope.dropdown.selected.model;
-				ngModelCtrl.$setViewValue( valueFn(scope, locals) );
-				ngModelCtrl.$render();
-			};
+			dropdownScope.$watch('current.selection', function(n) {
+				$timeout(function() {
+					element.val(n.index);
+					element.triggerHandler('change');
+				});
+			});
 
 		}
 	};
